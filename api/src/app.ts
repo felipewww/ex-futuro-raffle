3b9655fe58227f1a0f6b7afbd8fdecc4975e7466
Felipe Barreiros <felipewww@outlook.com.br>
2024-09-12 16:18:42 -0300

    first commit

---
+++ api/src/app.ts
@@ -0,0 +1,5 @@
+import 'dotenv/config'
+import 'source-map-support/register'
+require('source-map-support').install();
+require('module-alias/register')
+
---
+++ api/src/controller/raffles.routes.ts
@@ -0,0 +1,4 @@
+import {RequestRaffleUseCase} from "@/domain/request-raffle.use-case";
+
+new RequestRaffleUseCase()
+
